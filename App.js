8660070e96105b1d03235d7ad093cd1ffc3f08e0
ThomasThoams <thomasmiramont@outlook.fr>
2024-03-01 14:29:24 +0100

    commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+//App.js
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
---
+++ src/screen/login.jsx
@@ -0,0 +1,52 @@
+// ReacNativeTrello/src/screen/login.jsx
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Ici, vous pouvez implémenter la logique d'authentification
+    // Par exemple, en utilisant une API ou une méthode d'authentification tierce
+    // Une fois l'authentification réussie, vous pouvez naviguer vers la page de tableau de bord
+    navigation.navigate('Board');
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+
+export default Login;
