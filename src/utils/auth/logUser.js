d519d21e8a28fe295d996fadb6fea621377e3974
Mandfred GRONDIN <mandfred974@hotmail.fr>
2024-03-01 16:00:36 +0100

    add

--- src/utils/auth/logUser.js
+++ src/utils/auth/logUser.js
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./app"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/app"
 
 const ERROR_MESSAGES = {
     "auth/email-already-in-use": "L'adresse e-mail est déjà utilisée par un autre compte.",
@@ -8,17 +8,6 @@ const ERROR_MESSAGES = {
     "auth/user-not-found": "Aucun utilisateur trouvé avec cette adresse e-mail.",
     "auth/wrong-password": "Le mot de passe est incorrect."
 };
-
-export async function createUser(email, password) {
-    try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        return userCredential.user;
-    }
-    catch (error) {
-        const errorMessage = ERROR_MESSAGES[error.code] || "Une erreur s'est produite lors de la création de l'utilisateur.";
-        throw new Error(errorMessage);
-    }
-}
 
 export async function logUser(email, password) {
     try {
