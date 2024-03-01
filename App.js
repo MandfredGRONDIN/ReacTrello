b5ad42d99413477b4d30d6f5e19210a009a3a708
Mandfred GRONDIN <mandfred974@hotmail.fr>
2024-03-01 15:45:36 +0100

    homeScreen

--- App.js
+++ App.js
@@ -6,13 +6,17 @@ import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { UserContext } from './src/context/userContext';
 import UserTabs from './src/routes/userTabs';
+import Home from './src/screen/homeScreen';
 
 export default function App() {
   const [user, setUser] = useState()
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <NavigationContainer>
-        <UserTabs />
+      {user ?
+          <Home />
+          :
+          <UserTabs />}
       </NavigationContainer>
     </UserContext.Provider>
   );
--- src/screen/homeScreen.jsx
+++ src/screen/homeScreen.jsx
@@ -1,1 +1,23 @@
 // home.jsx
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { UserContext } from '../context/userContext';
+//import { globalStyles } from '../globalStyles/globalStyles';
+
+const Home = () => {
+    const { user, setUser } = useContext(UserContext)
+    const handleDeco = () => {
+        setUser(undefined)
+    }
+    return (
+        <View >
+            <Text>{user.email}</Text>
+            <TouchableOpacity  onPress={handleDeco}>
+                <Text >Deconnecter</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+
+export default Home;
