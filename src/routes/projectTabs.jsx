f9afcdd19ecb94a8bd62b8bc7b58b92073c68ef5
Mandfred GRONDIN <mandfred974@hotmail.fr>
2024-03-05 16:02:12 +0100

    icone

--- src/routes/projectTabs.jsx
+++ src/routes/projectTabs.jsx
@@ -1,22 +1,45 @@
-// src/routes/projectTabs.jsx
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons'; // Importez les icônes Ionicons
 import User from '../screen/UserScreen';
 import AddProject from '../screen/AddProjectScreen';
 import HomeScreen from '../screen/HomeScreen';
-
 
 const Tab = createMaterialBottomTabNavigator();
 
 const projectTabs = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name='Project' component={HomeScreen} initialParams={{ refresh: Math.random() }} />
-            <Tab.Screen name='AddProject' component={AddProject} />
-            <Tab.Screen name='User' component={User} />
+            <Tab.Screen 
+                name='Projets' 
+                component={HomeScreen} 
+                initialParams={{ refresh: Math.random() }} 
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="home" size={24} color={color} /> 
+                    ),
+                }}
+            />
+            <Tab.Screen 
+                name='Nouveau projet' 
+                component={AddProject} 
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="add-circle-outline" size={24} color={color} /> 
+                    ),
+                }}
+            />
+            <Tab.Screen 
+                name='Profil' 
+                component={User} 
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="person" size={24} color={color} /> 
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
-
 
 export default projectTabs;
